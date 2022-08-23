49effaa8f1319716a03f47d72720c74745e3140f
Ducluong <ducluong@MacBook-Pro-cua-Ducluong.local>
2022-08-23 14:37:14 +0700

    fix recorder

--- src/views/Ielts/speaking/components/RecordExam.jsx
+++ src/views/Ielts/speaking/components/RecordExam.jsx
@@ -1,11 +1,10 @@
 
-import AudioReactRecorder from "audio-react-recorder";
-import RecorderImageDeactive from "assets/image/speaking-exam/IST_icon_deactive.svg";
-import RecorderImageActive from "assets/image/speaking-exam/IST_icon_active.svg";
 import { makeStyles } from "@mui/styles";
-import Text from "components/Typography";
+import RecorderImageActive from "assets/image/speaking-exam/IST_icon_active.svg";
+import RecorderImageDeactive from "assets/image/speaking-exam/IST_icon_deactive.svg";
 import { CountDowns } from "components/Countdown";
-import { RecordStateEnum,part } from "interfaces/recorder";
+import Text from "components/Typography";
+import { part } from "interfaces/recorder";
 import { useMemo } from "react";
 
 const useStyles = makeStyles((theme)=>({
@@ -20,9 +19,6 @@ const useStyles = makeStyles((theme)=>({
     flexDirection:'column',
     gap:32
   },
-  recorder:{
-    display:"none"
-  }
 }))
 
 
@@ -30,16 +26,8 @@ const useStyles = makeStyles((theme)=>({
 
 const  Recorder =(props)=> {
   // !State
-  const {recordState,uploadVideo,partAnswering}=props
+  const {isRecording,partAnswering}=props
   const classes = useStyles()
-  
-  //audioData contains blob and blobUrl
-  //you can use this function to pass the blob staight away
-  //audioData.blob instead of just audioData to get a hold of the blob
-  console.log("partAnswering",partAnswering)
- const  onStop = (audioData) => {
-    console.log("audioData", audioData);
-  };
 
   const seconds = useMemo(()=>{
     if(partAnswering === part.part1){
@@ -52,19 +40,15 @@ const  Recorder =(props)=> {
       return 30000
     }
 
-  },[recordState])
+  },[isRecording])
 
     return (
       <div className={classes.container}>
-          {recordState?.recordState === RecordStateEnum.START ? <div className={classes.activeSpeaking}>
+          {isRecording ? <div className={classes.activeSpeaking}>
             <Text.Title>Recording ...</Text.Title>
             <img src={RecorderImageActive} alt=" Active Recorder Image  " />
             <CountDowns secondsPass={seconds}  />
           </div> : <img src={RecorderImageDeactive} alt="deactive Recorder Image" /> }
-        
-        <div className={classes.recorder}>
-        <AudioReactRecorder  canvasWidth={0} backgroundColor="none" state={recordState?.recordState} onStop={uploadVideo} />
-        </div>
       </div>
     );
   }
