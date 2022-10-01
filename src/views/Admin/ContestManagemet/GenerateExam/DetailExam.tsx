45246246ab71b9a2f9e03d0c88137ed3dc990504
tungvip2143 <83186487+tungvip2143@users.noreply.github.com>
2022-10-01 20:22:55 +0700

    update button back

--- src/views/Admin/ContestManagemet/GenerateExam/DetailExam.tsx
+++ src/views/Admin/ContestManagemet/GenerateExam/DetailExam.tsx
@@ -1,12 +1,13 @@
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Tab } from "@mui/material";
+import { Button, Stack, Tab } from "@mui/material";
 import { Box } from "@mui/system";
 import useGetExamDetail from "hooks/ContestManagemet/useGetExamDetail";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import Listening from "./component/Listening";
 import Reading from "./component/Reading";
 import "./style.scss";
+import UndoIcon from "@mui/icons-material/Undo";
 
 const DetailExam = () => {
   //!State
@@ -22,10 +23,20 @@ const DetailExam = () => {
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setTab(newValue);
   };
-
+  const history = useHistory();
+  const renderButtonBack = () => {
+    return (
+      <Stack spacing={2} direction="row" className="justify-end mb-[10px]">
+        <Button component="a" href="#as-link" startIcon={<UndoIcon />} onClick={() => history.goBack()}>
+          Back
+        </Button>
+      </Stack>
+    );
+  };
   return (
     <div className="examContainer">
       <Box sx={{ width: "100%", typography: "body1" }}>
+        <div>{renderButtonBack()}</div>
         <TabContext value={tab}>
           <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
             <TabList onChange={handleChange} aria-label="lab API tabs example">
